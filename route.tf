236b9ce045504313a9a22f97ed9bdb8cefb22df3
murugaraj-eng <murugaraj@msn.com>
2025-12-03 16:33:36 +0530

    Add route table and subnet attachment resources

---
+++ route.tf
@@ -0,0 +1,16 @@
+resource "oci_core_route_table" "my_rt" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_vcn.my_vcn.id
+  display_name   = "my-route-table"
+
+  route_rules {
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = oci_core_internet_gateway.my_igw.id
+  }
+}
+
+resource "oci_core_subnet_route_table_attachment" "attach_rt" {
+  subnet_id      = oci_core_subnet.my_subnet.id
+  route_table_id = oci_core_route_table.my_rt.id
+}
